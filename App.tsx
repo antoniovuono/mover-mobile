c0fa8b591c68599626627c0a5eed04fbec1328a0
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-02-06 21:40:15 -0300

    Project ready to be started

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Welcome from "./src/modules/Home/screens/Welcome";
+
+const App: React.FC = () => {
+    return <Welcome />;
+};
+
+export default App;
---
+++ src/modules/Home/screens/Welcome/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Title } from "./styles";
+
+const Welcome: React.FC = () => {
+    return (
+        <Container>
+            <Title>Welcome Mover</Title>
+        </Container>
+    );
+};
+
+export default Welcome;
