4979a63ab5e22a543a33b88a8831aafd9e8b30df
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-02-07 15:03:27 -0300

    Download google maps

--- src/modules/Home/screens/Welcome/index.tsx
+++ src/modules/Home/screens/Welcome/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import MapView from "react-native-maps";
 
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 const Welcome: React.FC = () => {
     return (
         <Container>
-            <Title>Welcome Mover</Title>
+            <MapView
+                style={{ width: "100%", height: "100%" }}
+                initialRegion={{
+                    latitude: -23.6013242,
+                    longitude: -48.0518921,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </Container>
     );
 };
