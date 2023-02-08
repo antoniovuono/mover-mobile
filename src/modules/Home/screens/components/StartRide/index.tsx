2f24e22640f2c3f2549d3eccb7bae246e9d9abcf
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-02-08 08:36:19 -0300

    Removing unsued content

--- src/modules/Home/screens/components/StartRide/index.tsx
+++ src/modules/Home/screens/components/StartRide/index.tsx
@@ -1,9 +1,21 @@
+import { Entypo } from "@expo/vector-icons";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
-import { Container } from "./styles";
+import { Container, ExpandContent } from "./styles";
 
 const StartRide: React.FC = () => {
-    return <Container />;
+    return (
+        <Container>
+            <ExpandContent>
+                <TouchableOpacity
+                    hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+                >
+                    <Entypo name="chevron-thin-up" size={17} color="black" />
+                </TouchableOpacity>
+            </ExpandContent>
+        </Container>
+    );
 };
 
 export default StartRide;
--- src/modules/Home/screens/components/StartRide/styles.ts
+++ src/modules/Home/screens/components/StartRide/styles.ts
@@ -1,9 +1,17 @@
+import { Platform } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     width: 85%;
-    height: 141px;
+    height: ${Platform.OS === "ios" ? 170 : 141}px;
     background-color: white;
     position: absolute;
     bottom: 30px;
+    border-radius: 4px;
+`;
+
+export const ExpandContent = styled.View`
+    padding: 8px;
+    justify-content: center;
+    align-items: center;
 `;
