c0f9e2bf5fe1bffc79891289240df0717daa4c84
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-02-10 09:11:34 -0300

    updating input style

--- src/components/SearchLocationInput/index.tsx
+++ src/components/SearchLocationInput/index.tsx
@@ -1,9 +1,8 @@
-import { Entypo } from "@expo/vector-icons";
 import React from "react";
 import { TextInputProps } from "react-native";
 import { useTheme } from "styled-components";
 
-import { Input, InputContent } from "./styles";
+import { Input } from "./styles";
 
 interface IInputProps extends TextInputProps {
     placeholder: string;
@@ -16,18 +15,11 @@ const SearchLocationInput: React.FC<IInputProps> = ({
     const theme = useTheme();
 
     return (
-        <InputContent>
-            <Entypo
-                name="location"
-                size={17}
-                color={theme.colors.primary_gray}
-            />
-            <Input
-                placeholder={placeholder}
-                {...rest}
-                placeholderTextColor={theme.colors.primary_gray}
-            />
-        </InputContent>
+        <Input
+            placeholder={placeholder}
+            {...rest}
+            placeholderTextColor={theme.colors.primary_gray}
+        />
     );
 };
 
--- src/components/SearchLocationInput/styles.ts
+++ src/components/SearchLocationInput/styles.ts
@@ -1,20 +1,12 @@
 import styled from "styled-components/native";
 
-export const InputContent = styled.View`
-    flex-direction: row;
-    justify-content: center;
-    margin-bottom: 11px;
-    align-items: center;
-`;
-
 export const Input = styled.TextInput`
-    background-color: red;
+    width: 100%;
     height: 45px;
     background-color: ${({ theme }) => theme.colors.input_blue};
     border-radius: 4px;
     padding: 0 15px;
-    margin-left: 10px;
-    flex: 1;
     font-size: 16px;
     color: ${({ theme }) => theme.colors.primary_title_dark};
+    margin-bottom: 6px;
 `;
